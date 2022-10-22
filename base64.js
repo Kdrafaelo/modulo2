e51f81d086c7ef5371a8363060493772f510ec49
Kdrafaelo <114005063+Kdrafaelo@users.noreply.github.com>
2022-10-21 23:23:49 -0300

    Add files via upload

---
+++ base64.js
@@ -0,0 +1,13 @@
+
+ function base64Encoder(normalText) {
+    const encondedText = btoa(normalText);
+  
+    return encondedText;
+  }
+  
+
+  function base64Decoder(encodedText) {
+    const normalText = atob(encodedText);
+  
+    return normalText;
+  }
